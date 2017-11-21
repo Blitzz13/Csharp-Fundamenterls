efbb17c2945e5135be05ba4ac0425695a145e2bf
Ivan <thebeardman123@gmail.com>
2017-11-21 16:21:03 +0200

    Person Problem Solved

---
+++ Inheritance/Person/Child.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Person
+{
+	public class Child:Person
+	{
+
+		public Child(string _name,int _age)
+			:base(_name,_age)
+		{
+			
+		}
+
+		public override int Age
+		{
+			get { return base.Age; }
+			set
+			{
+				if (value > 15)
+				{
+					throw new ArgumentException("Child\'s age must be less than 15!");
+				}
+				base.Age = value;
+			}
+		}
+	}
+}
---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+	public class Person
+	{
+		public string _name;
+		public int _age;
+
+		public Person(string _name, int _age)
+		{
+			this.Name = _name;
+			this.Age = _age;
+		}
+
+		public virtual string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (value.Length < 3)
+				{
+					throw new ArgumentException("Name's length should not be less than 3 symbols!");
+				}
+				_name = value;
+			}
+		}
+
+		public virtual int Age
+		{
+			get { return _age; }
+			set
+			{
+				if (value < 0)
+				{
+					throw new ArgumentException("Age must be positive!");
+				}
+
+				this._age = value;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"Name: {this.Name}, Age: {this.Age}";
+		}
+
+	}
+}
