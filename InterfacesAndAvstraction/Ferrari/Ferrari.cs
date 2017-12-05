00b7f2292565b39dad163bf061f829e4f37113e3
Ivan <thebeardman123@gmail.com>
2017-12-05 13:00:33 +0200

    Problems 2 and 3 solved from interface lecture

---
+++ InterfacesAndAvstraction/Ferrari/Ferrari.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari
+{
+	class Ferrari : IFerrari
+	{
+		public Ferrari(string driver)
+		{
+			this.Model = "488-Spider";
+			this.Driver = driver;
+			this.Brakes = "Brakes!";
+			this.GasPedal = "Zadu6avam sA!";
+		}
+
+		public string Model { get; set; }
+		public string Driver { get; set; }
+		public string GasPedal { get; set; }
+		public string Brakes { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Model}/{Brakes}/{GasPedal}/{Driver}";
+		}
+	}
+}
---
+++ InterfacesAndAvstraction/Ferrari/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ferrari
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name = Console.ReadLine();
+			var ferrari = new Ferrari(name);
+
+			Console.WriteLine(ferrari.ToString());
+		}
+	}
+}
